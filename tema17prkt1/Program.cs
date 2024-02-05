6edf7b72374220641f2e30003d670f9537283ba5
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-02-05 09:56:09 +0200

    Add files via upload

---
+++ tema17prkt1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace tema17prkt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Артём","Мутаф",17);
+            person.Print();
+            Employee employee = new Employee("Microsoft","Programmier");
+            employee.Print();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
