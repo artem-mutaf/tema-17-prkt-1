a4740b590d7523bad434f1fb5e9fd7810ac6b7f5
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-02-09 14:26:48 +0200

    Add files via upload

---
+++ tema17prkt1(2)/Maga.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema17prkt1_2_
+{
+    class Maga:Press
+    {
+        string quality;
+        public string Quality
+        {
+            get { return quality; }
+            set { quality = value; }
+        }
+
+        public Maga() { }
+        public Maga(string quality)
+        {
+            this.quality = quality;
+        }
+        public new double Cost()
+        {
+            
+            if(quality=="Высокое")
+            {
+                return base.Cost() + ((base.Cost() / 100) * 10);
+            }
+            else if(quality=="Низкое")
+            {
+                return base.Cost() + ((base.Cost() / 100) * -10);
+            }
+            else
+            {
+                return base.Cost();
+            }
+        }
+    }
+}
---
+++ tema17prkt1(2)/Press.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace tema17prkt1_2_
+{
+    class Press
+    {
+        int copiess;
+        string name;
+        double price;
+
+        public int Copiess
+        {
+            get { return copiess; }
+            set { copiess = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public Press() { }
+        public Press(int copiess,string name,double price)
+        {
+            this.copiess = copiess;
+            this.name = name;
+            this.price = price;
+        }
+
+        public double Cost()
+        {
+            return price*copiess ;
+        }
+        virtual public void OutPut()
+        {
+            WriteLine($" Стоимость тиража: {Cost()} \nКол-во копий: {copiess}\nНазвание: {name}\nЦена: {price}");
+        }
+    }
+}
