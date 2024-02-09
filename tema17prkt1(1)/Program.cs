23a91a08e297d09cecee1c01dca6cc0addeddd33
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-02-09 13:35:50 +0200

    Add files via upload

---
+++ tema17prkt1(1)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace tema17prkt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Артём", "Мутаф", 17);
+            Student student = new Student("Артём", "Мутаф", 17, 2);
+            student.Print();
+            Employee employee = new Employee("Артём", "Мутаф", 17, "Microsoft", "Programmier");
+            employee.Print();
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person("Артём","Мутаф",17));
+            persons.Add(new Employee("Артём", "Мутаф", 17, "Microsoft", "Programmier"));
+            persons.Add(new Student("Артём", "Мутаф", 17,2));
+            foreach(var sigma in persons)
+            {
+                sigma.Print();
+            }
+
+            ReadKey();
+        }
+    }
+}
